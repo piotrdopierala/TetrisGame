8b48e3ee6a34a0cab18186934d44327d48a99b38
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-10-31 11:51:02 +0100

    refactor in TetrisElements ENUM.

--- src/TetrisGame_PD/Main/GameLogic/Element/TetrisElements.java
+++ src/TetrisGame_PD/Main/GameLogic/Element/TetrisElements.java
@@ -5,39 +5,42 @@ import TetrisGame_PD.Main.GameLogic.BlockDef;
 import java.util.*;
 
 public enum TetrisElements {
-    I('I'), J('J'), L('L'), O('O'), S('S'), T('T'), Z('Z');
+    I(), J(), L(), O(), S(), T(), Z();
 
-    private char type;
+    //private char type;
     private static final List<TetrisElements> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
     private static final int SIZE = VALUES.size();
     private static final Random rnd = new Random();
 
     TetrisElements(char z) {
-        type = z;
+
+    }
+
+    TetrisElements() {
     }
 
     public int[][] getArray() {
         int a=BlockDef.RUNNING_BLOCK;
-        switch (type) {
-            case 'I':
+        switch (this.name()) {
+            case "I":
                 a= BlockDef.RUNNING_BLOCK_BLUE;
                 return new int[][]{{a, 0, 0, 0}, {a, 0, 0, 0}, {a, 0, 0, 0}, {a, 0, 0, 0}};
-            case 'J':
+            case "J":
                 a= BlockDef.RUNNING_BLOCK_YELLOW;
                 return new int[][]{{a, a, a, 0}, {0, 0, a, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-            case 'L':
+            case "L":
                 a= BlockDef.RUNNING_BLOCK_VIOLET;
                 return new int[][]{{a, a, a, 0}, {a, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-            case 'O':
+            case "O":
                 a= BlockDef.RUNNING_BLOCK_GREEN;
                 return new int[][]{{a, a, 0, 0}, {a, a, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-            case 'S':
+            case "S":
                 a= BlockDef.RUNNING_BLOCK_ORANGE;
                 return new int[][]{{0, a, a, 0}, {a, a, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-            case 'T':
+            case "T":
                 a= BlockDef.RUNNING_BLOCK_RED;
                 return new int[][]{{a, a, a, 0}, {0, a, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-            case 'Z':
+            case "Z":
                 a= BlockDef.RUNNING_BLOCK_BROWN;
                 return new int[][]{{a, a, 0, 0}, {0, a, a, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
             default:
